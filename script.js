5e403595b23845c0e1dc9f5256e795aa8b96b5c7
tifa <latifaprses@outlook.com>
2025-02-03 10:33:55 +0100

    set scores to 0 and hide the dice

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+//selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.querySelector('#score--1');
+const dice = document.querySelector('.dice');
+
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add('hidden');
